f23abd756b1c24cf637962be927c9a774a437cba
Avik Sinha <aviksinha941@gmail.com>
2025-04-15 00:54:53 +0530

    improved the MealItem Widget

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -17,7 +17,7 @@ class CategoryGridItem extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
         onTap: onSelectCategory,
-        splashColor: Colors.white,
+        splashColor: Theme.of(context).primaryColor,
         borderRadius: BorderRadius.circular(16),
         child: Container(
           padding: EdgeInsets.all(16),
@@ -26,8 +26,10 @@ class CategoryGridItem extends StatelessWidget {
             gradient: LinearGradient(
               colors: [
                 //category.color.withValues(alpha: (0.5 * 255).toInt())
-                category.color.withOpacity(0.55),
-                category.color.withOpacity(0.9),
+                // category.color.withOpacity(0.55),
+                // category.color.withOpacity(0.9),
+                category.color.withAlpha((0.55 * 255).toInt()),
+                category.color.withAlpha((0.9 * 255).toInt()),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -9,7 +9,11 @@ class MealItemTrait extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [Icon(icon, size: 17, color: Colors.white), SizedBox()],
+      children: [
+        Icon(icon, size: 17, color: Colors.white),
+        SizedBox(width: 6),
+        Text(label, style: TextStyle(color: Colors.white)),
+      ],
     );
   }
 }
